b3e34b6629afd9115d90e2a488b46dd311098c68
Lucas Finoti Rodrigues <lucas-finoti@lucas.rodrigues>
2024-08-15 13:48:27 -0300

    first commit

---
+++ src/main/java/com/lucas/aws_projeto1/AwsProjeto1Application.java
@@ -0,0 +1,13 @@
+package com.lucas.aws_projeto1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjeto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjeto1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/aws_projeto1/AwsProjeto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.aws_projeto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjeto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
